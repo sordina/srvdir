1baf9c9f43a8cb6c008ed92123f5c9d864711beb
Lyndon Maydwell <lyndon@sordina.net>
2017-01-19 21:52:45 +1100

    Removing servant dependency; adding options

--- app/Main.hs
+++ app/Main.hs
@@ -5,31 +5,38 @@ module Main where
 
 import Data.Maybe
 import Options.Generic
-import Servant.Utils.StaticFiles
-import Servant.API.Raw
-import Servant.Server
-import Servant
+import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Application.Static
+import System.FilePath
+
 
 data Options = Options
-             { port      :: Port
-             , verbose   :: Bool
-             , directory :: Maybe FilePath
+             { port           :: Port
+             , verbose        :: Bool
+             , disableListing :: Bool
+             , directory      :: Maybe FilePath
              } deriving (Show, Generic)
 
 instance ParseRecord Options
 
-server :: FilePath -> Application
-server f = serve (Proxy :: Proxy Raw) (serveDirectory f)
+serveDirectory :: Options -> FilePath -> Application
+serveDirectory o = staticApp . settings . addTrailingPathSeparator
+  where
+  settings f = setting { ssListing = listing }
+    where
+    setting = defaultFileServerSettings f
+    listing        | disableListing o = Nothing
+                   | otherwise        = ssListing setting
 
 runServer :: Options -> IO ()
 runServer o = run p s
   where
   p = port o
   d = fromMaybe "." (directory o)
-  s | verbose o = logStdoutDev (server d)
-    | otherwise = server d
+  s | verbose o = logStdoutDev (serveDirectory o d)
+    | otherwise = serveDirectory o d
 
 main :: IO ()
-main = runServer =<< getRecord "srvdir - simple servant directory server!"
+main = runServer =<< getRecord "srvdir - simple directory server!"
