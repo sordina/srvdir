f2f9a5cc149a5edf10d21ad67cb8f2b11f2fdfb1
Lyndon Maydwell <lyndon@sordina.net>
2021-10-20 19:05:31 +1100

    more robust

---
+++ app/ExceptionMiddleware.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module ExceptionMiddleware
+    ( handleException
+    , handleExceptionPure
+    , errorOnException
+    , errorOnSomeException
+    ) where
+
+import Network.Wai ( responseLBS, Middleware, Request, Response )
+import Control.Exception ( SomeException, catch, Exception )
+import Data.Data ( Proxy(..) )
+import Network.HTTP.Types.Status ( internalServerError500 )
+
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+-- | General exception handler. Can respond to exception and request to perform IO actions and construct a response.
+--
+-- Exception types are controlled by a `Proxy` argument.
+--
+handleException :: Exception e => Proxy e -> (Request -> e -> IO Response) -> Middleware
+handleException (_proxy :: Proxy e) f app req send = do
+  catch (app req send) $ \(e :: e) -> do
+    res <- f req e
+    send res
+
+-- | General exception handler. Can respond to exception and request to construct a response.
+--
+-- Exception types are controlled by a `Proxy` argument.
+--
+handleExceptionPure :: Exception e => Proxy e -> (Request -> e -> Response) -> Middleware
+handleExceptionPure p f = handleException p (\r e -> pure $ f r e)
+
+-- | Exceptions are turned into error responses.
+--
+-- Exception types are controlled by a `Proxy` argument.
+--
+errorOnException :: Exception e => Proxy e -> Middleware
+errorOnException p = handleExceptionPure p (\_r e -> responseLBS internalServerError500 [] (BS.pack (show e)))
+
+-- | Exceptions are turned into error responses.
+--
+-- Catches all `SomeException` type exceptions.
+--
+errorOnSomeException :: Middleware
+errorOnSomeException = errorOnException (Proxy :: Proxy SomeException)
+
--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,9 @@ import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Application.Static
 import System.FilePath
-
+import ExceptionMiddleware
+import Data.Default (def)
+import System.IO.Unsafe (unsafePerformIO)
 
 data Options = Options
              { port           :: Port
@@ -20,6 +22,9 @@ data Options = Options
              } deriving (Show, Generic)
 
 instance ParseRecord Options
+
+mkLogger :: IO Middleware
+mkLogger = mkRequestLogger def { outputFormat = DetailedWithSettings (def { mPrelogRequests = True }) }
 
 serveDirectory :: Options -> FilePath -> Application
 serveDirectory o = staticApp . settings . addTrailingPathSeparator
@@ -31,12 +36,14 @@ serveDirectory o = staticApp . settings . addTrailingPathSeparator
                    | otherwise        = ssListing setting
 
 runServer :: Options -> IO ()
-runServer o = run p s
+runServer o = do
+  logger <- mkLogger
+  run p (s logger)
   where
   p = port o
   d = fromMaybe "." (directory o)
-  s | verbose o = logStdoutDev (serveDirectory o d)
-    | otherwise = serveDirectory o d
+  s log | verbose o = log (errorOnSomeException (serveDirectory o d))
+        | otherwise = serveDirectory o d
 
 main :: IO ()
 main = runServer =<< getRecord "srvdir - simple directory server!"
